dc399a08af33d9c2d5d29f3589cc5c02973318e1
jwchavarrro <jwchavarro@prevalentware.com>
2025-11-05 14:40:46 -0500

    Refactor routing to replace Inicio component with HomeComponent and add new routes for Contact, Experience, and Skills components. Remove unused Inicio page files including HTML, SCSS, and TypeScript.

--- src/app/pages/inicio/inicio.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  templateUrl: './inicio.html',
-  styleUrl: './inicio.scss'
-})
-export class Inicio {
-}
